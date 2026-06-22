7baa8f8acf2730d36f1cc883e86a7089666b0303
오용준 <dhdydwns723@gmail.com>
2026-06-22 15:37:03 +0900

    토마토 정보 get, post 기본 설정 메소드 추가

--- src/main/java/java/suhaeng/demo/controller/TomatoController.java
+++ src/main/java/java/suhaeng/demo/controller/TomatoController.java
@@ -2,10 +2,23 @@ package java.suhaeng.demo.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class TomatoController {
+    @GetMapping("/get/tomato-record")
+    public Object getTomatoRecord() {
+        log.info("get tomato-record");
+        return "get: tomato-record";
+    }
 
+    @PostMapping("/post/tomato-state/{date}")
+    public String postTomatoState(@PathVariable("date") int date) {
+        log.info("post tomato-state || " + date);
+        return "post: tomato-state";
+    }
 }

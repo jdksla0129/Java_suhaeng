5324380dcd6976e70833caf89c4baa1ece35ea36
오용준 <dhdydwns723@gmail.com>
2026-06-23 11:03:55 +0900

    생성시 반환값이 오는 dto 추가

--- src/main/java/suhaeng/demo/domain/tomato/dto/response/CreateTomatoResponse.java
+++ src/main/java/suhaeng/demo/domain/tomato/dto/response/CreateTomatoResponse.java
@@ -1,5 +1,8 @@
 package suhaeng.demo.domain.tomato.dto.response;
 
+import suhaeng.demo.domain.tomato.entity.Tomato;
+import suhaeng.demo.domain.tomato.enumeration.Weather;
+
 public record CreateTomatoResponse(
         String title,
         int date,
@@ -7,10 +10,23 @@ public record CreateTomatoResponse(
         String content,
         int wateringCount,
         String fruit,
-        String weather,
+        Weather weather,
         int temperature,
         int leafCount,
         boolean pruned
 ) {
-
+    public static CreateTomatoResponse from(Tomato tomato) {
+        return new CreateTomatoResponse(
+                tomato.getTitle(),
+                tomato.getDate(),
+                tomato.getTime(),
+                tomato.getContent(),
+                tomato.getWateringCount(),
+                tomato.getFruit(),
+                tomato.getWeather(),
+                tomato.getTemperature(),
+                tomato.getLeafCount(),
+                tomato.isPruned()
+        );
+    }
 }

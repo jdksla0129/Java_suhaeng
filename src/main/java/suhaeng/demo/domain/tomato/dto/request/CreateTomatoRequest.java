b04f0e107506abe4616ddeb876cb46bbdb48be8b
오용준 <dhdydwns723@gmail.com>
2026-06-26 11:20:46 +0900

    무결성 요소 추가

--- src/main/java/suhaeng/demo/domain/tomato/dto/request/CreateTomatoRequest.java
+++ src/main/java/suhaeng/demo/domain/tomato/dto/request/CreateTomatoRequest.java
@@ -1,10 +1,14 @@
 package suhaeng.demo.domain.tomato.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import suhaeng.demo.domain.tomato.enumeration.Dust;
 import suhaeng.demo.domain.tomato.enumeration.Weather;
 
 public record CreateTomatoRequest(
+        @NotBlank
         String title,
+        @NotNull
         int date,
         String time,
         String content,

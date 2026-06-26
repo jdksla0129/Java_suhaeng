fa247bbd3ba184d029c06f4ee0e7c54116af3214
오용준 <dhdydwns723@gmail.com>
2026-06-26 11:04:23 +0900

    Request DTO 수정

--- src/main/java/suhaeng/demo/domain/tomato/dto/request/CreateTomatoRequest.java
+++ src/main/java/suhaeng/demo/domain/tomato/dto/request/CreateTomatoRequest.java
@@ -1,6 +1,19 @@
 package suhaeng.demo.domain.tomato.dto.request;
 
+import suhaeng.demo.domain.tomato.enumeration.Dust;
+import suhaeng.demo.domain.tomato.enumeration.Weather;
+
 public record CreateTomatoRequest(
-        int date
+        String title,
+        int date,
+        String time,
+        String content,
+         int wateringCount,
+         String fruit,
+         Weather weather,
+         int temperature,
+         int leafCount,
+         boolean pruned,
+         Dust dustConcentration
 ) {
 }
